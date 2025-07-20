daeb310c4507cb4ce5916452b4ae2c95113e595a
alma1969 <alma1969@gmail.com>
2025-07-19 19:34:39 -0600

    Cambio de texto en Header para prueba de despliegue continuo

--- mi-proyecto-web/src/components/Header/Header.jsx
+++ mi-proyecto-web/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@
    return (
      <header className={styles.header}>
        <div className={styles.container}>
-         <h1>Mi Sitio</h1>
+         <h1>Hola desde Netlify con despliegue continuo</h1>
          <nav>...</nav>
        </div>
      </header>
